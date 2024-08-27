17734f25e376571ecdb4c4bce6b85f08ce294d83
ibrahim11elian <ibrahim11elian@gmail.com>
2024-08-27 03:09:18 +0300

    Add almost every courses routes.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\RestrictedTo;
 use Illuminate\Foundation\Application;
@@ -26,8 +27,26 @@ Route::middleware('auth')->group(function () {
 });
 
 
-Route::get('/admin',function(){
-    return Inertia::render('Admin');
-})->middleware(['auth', 'restricted_to:student']);
+// Route::get('/admin',function(){
+//     return Inertia::render('Admin');
+// })->middleware(['auth', 'restricted_to:student']);
+
+Route::controller(CourseController::class)->group(function(){
+    Route::get('/courses', 'index')->name('courses.index');
+    Route::get('/courses/{course}', 'show')->name('courses.show');
+
+    Route::middleware(['auth','restricted_to:instructor'])->group(function(){
+
+        Route::get('/courses/create', 'create')->can('create')->name('courses.create');
+        Route::post('/courses', 'store')->can('create')->name('courses.store');
+
+
+        Route::get('/courses/{course}/edit', 'edit')->can('update')->name('courses.edit');
+        Route::patch('/courses/{course}', 'update')->can('update')->name('courses.update');
+
+        Route::delete('/courses/{course}', 'destroy')->can('delete')->name('courses.destroy');
+    });
+
+});
 
 require __DIR__.'/auth.php';

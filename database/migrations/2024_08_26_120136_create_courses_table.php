8b8a7eebbdcdd689cf14bb6826882a05062ed767
ibrahim11elian <ibrahim11elian@gmail.com>
2024-08-28 18:18:42 +0300

    update the course table structure to latest changes

--- database/migrations/2024_08_26_120136_create_courses_table.php
+++ database/migrations/2024_08_26_120136_create_courses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->foreignId('instructor_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('instructor_id')->references('id')->on('instructors')->cascadeOnDelete();
             $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete();
             $table->timestamps();
         });

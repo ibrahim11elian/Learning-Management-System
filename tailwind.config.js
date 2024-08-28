4ee86b28401d0b65f4c91b8cbed8d8e122e9853c
ibrahim11elian <ibrahim11elian@gmail.com>
2024-08-28 18:12:15 +0300

    Added some colors from the design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,33 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+
+            colors: {
+                main: {
+                    100: "#FFEEE8",
+                    200: "#FFDDD1",
+                    300: "#FFA386",
+                    400: "#FF855E",
+                    500: "#FF6636",
+                    600: "#CC522B",
+                    700: "#993D20",
+                    800: "#662916",
+                    900: "#33140B",
+                },
             },
         },
     },

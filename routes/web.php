27df290074fc48de826b2df98d7589db6d889e7f
ibrahim11elian <ibrahim11elian@gmail.com>
2024-08-28 18:21:23 +0300

    Changed the root route to home for now

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,18 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
+Route::get('/',function(){
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
+    return Inertia::render('Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
